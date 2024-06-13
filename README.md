aafd8eef57cb9d9afb905a242d2a78e32dc0e5ca
Marcélio G. Pereira <marcelio.pereira@tse.jus.br>
2024-06-13 00:46:15 -0300

    Aula04 - Concluído aula 4, curso 2

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Projeto da Aula de Javascript da Alura
 - Aula 01 - Conhecendo o Javascript.
 - Aula 02 - Funções.
 - Aula 03 - Listas.
+- Aula 04 - Iterando em listas
--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;
+
+//listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+//enquanto
+//while (contador < listaDeTeclas.length) {
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const audio = `#som_${instrumento}`;     //template string
+
+    tecla.onclick = function () {
+        tocaSom(audio);
+    }
+    
+    //contador++;
+    
+    console.log(contador);
+}
