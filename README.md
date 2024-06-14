23e89eb97316258a3660b70c58ae92c814590f61
Marcélio G. Pereira <marcelio.pereira@tse.jus.br>
2024-06-14 00:56:53 -0300

    Aula05 - diferenças entre tratamento da técla pressionada event.code e event.key

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Projeto da Aula de Javascript da Alura
 - Aula 01 - Conhecendo o Javascript.
 - Aula 02 - Funções.
 - Aula 03 - Listas.
-- Aula 04 - Iterando em listas
-- Aula 05 - Eventos e Lógica
+- Aula 04 - Iterando em listas.
+- Aula 05 - Eventos e Lógica.
--- main.js
+++ main.js
@@ -15,4 +15,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function (evento) {
+        console.log(evento);
+        //if (evento.code === 'Space' || evento.code === 'Enter' || evento.code ==='NumpadEnter') {
+        if (evento.key === ' ' || evento.key === 'Enter') {
+                tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
